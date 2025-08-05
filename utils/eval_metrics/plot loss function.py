b14c985b43ee23d895a2d95fe507c3afb8558ada
Mohankrish31 <134041145+Mohankrish31@users.noreply.github.com>
2025-08-05 16:54:14 +0530

    Create plot loss function.py

---
+++ utils/eval_metrics/plot loss function.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+plt.figure(figsize=(10, 6))
+plt.plot(train_losses, 'o-', label='Train Loss')
+plt.plot(val_losses, 's-', label='Validation Loss')
+plt.xlabel("Epoch Number", fontsize=14)
+plt.ylabel("Total Loss", fontsize=14)
+plt.title("Training and Validation Loss Curve for cbamrdbunet", fontsize=16)
+plt.legend(loc="upper right", fontsize=12)
+plt.grid(True)
+plt.tight_layout()
+plt.savefig("training_validation_loss_curve.png", dpi=300)
+plt.show()
